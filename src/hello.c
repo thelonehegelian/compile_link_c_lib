3eb616c0cd22b82c5845079efc1171814dc44812
umair iftikhar <umair.iftikhar@pulsar-systems.fi>
2023-01-11 01:43:51 +0000

    Add c program

---
+++ src/hello.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+
+
+void hello() {
+    printf("We don't say hello, we just say Affirmative!\n");
+}
+
+void greet(const char* name) {
+    printf("Hello, %s, human scum!\n", name);
+}
